517068817405ad952b6b700ea49679331b607e61
brucean52 <bruce.an52@gmail.com>
2018-02-09 16:56:14 -0800

    setup user-auth and do axios post to db

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const authRouter = require('./routes/auth_routes');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+
+mongoose.connect(keys.db_connect).then( () => {
+    console.log("connected to mongoDB");
+}).catch( error => {
+    console.log("connected to mongoDB", error.message);
+});
+
+app.use(cors());
+
+app.use(express.json());
+
+authRouter(app);
 
 app.get('/', (req, res) => {
         res.send('<h1>App is working!</h1>');
